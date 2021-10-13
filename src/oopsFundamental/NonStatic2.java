de4206281d6cf6d51b4468efd0fa56b241ef2caf
Dayananda <dayamn123@gmail.com>
2021-10-13 09:14:05 +0530

    New ststic 2 class added

---
+++ src/oopsFundamental/NonStatic2.java
@@ -0,0 +1,10 @@
+package oopsFundamental;
+
+public class NonStatic2 {
+	int a=10;
+public static void main(String[] args) {
+	System.out.println("hi");
+	
+	System.out.println(new NonStatic2().a);
+}
+}
